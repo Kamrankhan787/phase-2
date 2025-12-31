ab3efeb10e51cdb23049c34418f98eabe295cd09
Kamran Khan <kamrankhan787@users.noreply.github.com>
2025-12-31 21:39:25 +0500

    Add new feature or fix

--- main.py
+++ main.py
@@ -1,22 +1,13 @@
-import sys
-from todo_app.services.todo_service import TodoService
-from todo_app.cli.menu import Menu
+from src.todo_app.services.todo_service import TodoService
+from src.todo_app.cli.menu import CLIHandler
 
 def main():
-    """
-    Application entry point.
-    Satisfies Phase I Plan §1.
-    """
+    service = TodoService()
+    app = CLIHandler(service)
     try:
-        service = TodoService()
-        app = Menu(service)
         app.run()
     except KeyboardInterrupt:
-        print("\nProgram interrupted by user. Exiting.")
-        sys.exit(0)
-    except Exception as e:
-        print(f"An unexpected error occurred: {e}")
-        sys.exit(1)
+        print("\nExiting...")
 
 if __name__ == "__main__":
     main()
