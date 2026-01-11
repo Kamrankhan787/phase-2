5a8f7956b7cc40b4b39d51e5ef00dae714fe0b68
Kamran Khan <kamrankhan787@users.noreply.github.com>
2026-01-12 02:27:38 +0500

    Fix Vercel build: Downgrade to Tailwind stable v3 and fix PostCSS config

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "de0355f62965e06a"; }
+/******/ 		__webpack_require__.h = function() { return "22926261e5c1724d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
